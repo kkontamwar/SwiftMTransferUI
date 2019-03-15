a742b17a4a1157a6667a3feafc59a39389ed433f
anant3007 <46474294+anant3007@users.noreply.github.com>
2019-03-15 15:20:40 +0530

    Added Registration Logic and call to Azure hosted service.

--- SwiftMTransfer_UI/ClientApp/src/app/home/home.component.ts
+++ SwiftMTransfer_UI/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-register',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  registerForm: FormGroup;
+  name = new FormControl('');
+  clickMessage = '';
+  http: HttpClient;
+  baseUrl: string;
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string, private formBuilder: FormBuilder, private router: Router) {
+    this.http = http;
+    this.baseUrl = baseUrl;
+  }
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      mNumber: ['', Validators.required],
+      emailID: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get function() { return this.registerForm.controls; }
+
+  onSubmit() {
+    console.log(this.registerForm);
+    const url = "https://swiftmtransferapi.azurewebsites.net/api/User";
+
+
+    this.http.get(url).subscribe(result => {
+      console.log("Value returned from web service call is ", result),
+        this.name.setValue(result);
+      this.router.navigate(['/counter']);
+    }, error => console.error(error));
+
+
+    //this.http.post(this.baseUrl + 'api/SampleData/PostRegisterData', this.registerForm.value).subscribe(result => {
+    //  console.log("Value returned from web service call is ", result),
+    //    this.name.setValue(result);
+    //  this.router.navigate(['/counter']);
+    //}, error => console.error(error));
+
+  }
+
+
 }
